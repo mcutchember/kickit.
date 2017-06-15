a853b0377f790bc690a4f4d736d5fcb2646643a5
Myle$ <mcutchember@fullsail.edu>
2017-06-15 18:04:55 -0400

    initialCommit
    created project. created game menu. adds constrains

---
+++ kickit/kickit/How2ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  How2ViewController.swift
+//  kickit
+//
+//  Created by Myle$ on 6/15/17.
+//  Copyright © 2017 Myle$. All rights reserved.
+//
+
+import UIKit
+
+class How2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func okButton(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
