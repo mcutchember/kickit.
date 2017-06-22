5e056148837185a15a29d900dd20c877463ba0e9
christstuart <christstuart@Chriss-MacBook-Pro.local>
2017-06-22 00:17:05 -0400

    Added:
    Ads at the end of game, Gradient on HomeScreen and extra button on Game
    Over Alert.

--- kickit/kickit/AppDelegate.swift
+++ kickit/kickit/AppDelegate.swift
@@ -3,10 +3,12 @@
 //  kickit
 //
 //  Created by Myle$ on 6/14/17.
+//	Edited by Chris T Stuart
 //  Copyright © 2017 Myle$. All rights reserved.
 //
 
 import UIKit
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+		
+		 GADMobileAds.configure(withApplicationID: "ca-app-pub-7367066270682286~4854724057")
+		
         return true
     }
 
