49fd04759bc8325b567f6d8fe0abafc0124994ea
Myle$ <mcutchember@fullsail.edu>
2017-07-27 11:30:42 -0400

    collection is now saving images and it works in game

--- kickit/kickit/CharacterViewController.swift
+++ kickit/kickit/CharacterViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class CharacterViewController: UIViewController
 {
+
+    var enemyImage: UIImage?
+    var playerImage: UIImage?
+    
+    var defaults = UserDefaults.standard
+    var characterNum = 0
+    
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var interests = Characters.fetchInterests()
+    var character = Characters.fetchInterests()
     let cellScaling: CGFloat = 0.6
     
     override func viewDidLoad() {
@@ -31,6 +39,24 @@ class CharacterViewController: UIViewController
         collectionView?.dataSource = self
         collectionView?.delegate = self
     }
+    
+    @IBAction func okAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    @IBAction func resetButton(_ sender: Any) {
+        
+        let domain = Bundle.main.bundleIdentifier!
+        defaults.removePersistentDomain(forName: domain)
+        // UserDefaults.standard.synchronize()
+        
+        // set enemy back
+        //enemyButton.setImage(#imageLiteral(resourceName: "enemy"), for: .normal)
+        //playerButton.setImage(#imageLiteral(resourceName: "ball"), for: .normal)
+    }
 }
 
 extension CharacterViewController : UICollectionViewDataSource
@@ -40,14 +66,14 @@ extension CharacterViewController : UICollectionViewDataSource
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return interests.count
+        return character.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CharacterCell", for: indexPath) as! CharacterViewCell
         
-        cell.interest = interests[indexPath.item]
+        cell.interest = character[indexPath.item]
         
         return cell
     }
@@ -66,6 +92,75 @@ extension CharacterViewController : UIScrollViewDelegate, UICollectionViewDelega
         
         offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
         targetContentOffset.pointee = offset
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        var visibleRect = CGRect()
+        
+        visibleRect.origin = collectionView.contentOffset
+        visibleRect.size = collectionView.bounds.size
+        
+        let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
+        
+        let visibleIndexPath: IndexPath = collectionView.indexPathForItem(at: visiblePoint)!
+        
+        print("\(visibleIndexPath.row) YAHHH")
+        characterNum = visibleIndexPath.row
+        
+        
+        
+        switch characterNum {
+        case 0:
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "fire")), forKey: "enemyImage")
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "earth")), forKey: "playerImage")
+            print("Enemy image saved!")
+            print("Player image saved!")
+            break
+        case 1:
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "ghost")), forKey: "enemyImage")
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "girl")), forKey: "playerImage")
+            print("Enemy image saved!")
+            print("Player image saved!")
+            break
+        case 2:
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "devil")), forKey: "enemyImage")
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "angel")), forKey: "playerImage")
+            print("Enemy image saved!")
+            print("Player image saved!")
+            break
+        case 3:
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "cat")), forKey: "enemyImage")
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "mouse")), forKey: "playerImage")
+            print("Enemy image saved!")
+            print("Player image saved!")
+            break
+        case 4:
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "plate")), forKey: "enemyImage")
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "eat")), forKey: "playerImage")
+            print("Enemy image saved!")
+            print("Player image saved!")
+            break
+        case 5:
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "mink2")), forKey: "enemyImage")
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "mink")), forKey: "playerImage")
+            print("Enemy image saved!")
+            print("Player image saved!")
+            break
+        case 6:
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "lyle2")), forKey: "enemyImage")
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "julian1")), forKey: "playerImage")
+            print("Enemy image saved!")
+            print("Player image saved!")
+            break
+        case 7:
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "lyle")), forKey: "enemyImage")
+            self.defaults.set(UIImagePNGRepresentation(#imageLiteral(resourceName: "julian")), forKey: "playerImage")
+            print("Enemy image saved!")
+            print("Player image saved!")
+            break
+        default:
+            break
+        }
     }
 }
 
